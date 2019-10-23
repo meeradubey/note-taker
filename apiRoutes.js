4a6849b31224b1f5e836291d63edc990b83158aa
meera <meeradubey97>
2019-10-23 10:18:57 -0700

    added routes and readme

---
+++ apiRoutes.js
@@ -0,0 +1,89 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var noteData = require("../data/noteData");
+var Data = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function(req, res) {
+    res.json(noteData);
+  });
+
+  app.get("/api/data", function(req, res) {
+    res.json(Data);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (noteData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      Data.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    noteData.length = 0;
+    Data.length = 0;
+
+    res.json({ ok: true });
+  });
+};
+
+
+var getTables = function(){
+    $.ajax({
+        url: "/api/notes",
+        method: "GET",
+    }).then(function(response){
+        var table = response.blank
+        $("#noteShow").append(table)
+    });
+}
+
+var newTable = function(table){
+    $.ajax({
+        URL: "/api/notes",
+        menthod: "POST",
+        data: table,
+    });
+}
+
+var deleteAll = function(){
+    $.ajax({
+        url: "/api/clear",
+        method: "POST"
+    });
+};
---
+++ note.js
@@ -0,0 +1,12 @@
+var noteArray = [
+    {
+      NoteType: "Running Errands",
+      briefDescription: "grocery shop",
+      list: "buy bananas and milk",
+      date: "10/23/19"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = noteArray;
+  
